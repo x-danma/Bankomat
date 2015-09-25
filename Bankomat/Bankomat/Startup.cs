8288d965fe195da503979e640d62ea89d1e29721
x-danma <daniel.martensson89@outlook.com>
2015-09-25 16:14:28 +0200

    Created an ASP.NET MVC project

---
+++ Bankomat/Bankomat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bankomat.Startup))]
+namespace Bankomat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
