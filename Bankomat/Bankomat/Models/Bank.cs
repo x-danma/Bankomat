afa2d0b783bd7931212d8d13004e3ae49e721680
Ferhat <ferhad.sonmez@gmail.com>
2015-09-28 10:51:22 +0200

    Commited Bank class

---
+++ Bankomat/Bankomat/Models/Bank.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bankomat.Models
+{
+    public class Bank
+    {
+        public string BankName { get; set; }
+    }
+}
