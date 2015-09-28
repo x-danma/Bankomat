5a9e5391b4718b35b0d60f75d6110020a52d5a57
Ferhat <ferhad.sonmez@gmail.com>
2015-09-28 16:27:22 +0200

    Commited a unfinished CardLogin

--- Bankomat/Bank/Card.cs
+++ Bankomat/Bank/Card.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Bank
 {
@@ -19,5 +20,58 @@ namespace Bank
 
 
         public Account Account { get; set; }
+
+
+
+        private void CardLogin()
+        {
+
+            SqlConnection myConnection = new SqlConnection();
+            SqlCommand myCommand = new SqlCommand();
+
+            myConnection.ConnectionString = @"Data Source= localhost\SQLEXPRESS;Initial Catalog=Contacts;Integrated Security=SSPI";
+
+            SqlDataReader myReader = null;
+            try
+            {
+                
+                //öppnar en connection och startar SQL-commands för att selecta
+                myConnection.Open();
+                myCommand.Connection = myConnection;
+                myCommand.CommandText = "sp_SelectCardNumber";
+                myCommand.CommandText = "sp_SelectPinNumber";
+                myReader = myCommand.ExecuteReader();
+
+                while (myReader.Read())
+                {
+                    //Input från View:n
+
+                   // string cardNumberInput = textbox1.Text;
+                    //string pinInput = textbox2.Text;
+
+                   // if (CardNumberinput == CardNumber && pinInput == Pin )
+                    {
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            finally
+            {
+                if (myReader != null) myReader.Close();
+                if (myConnection != null) myConnection.Close();
+            }
+
+          
+
+
+
+
+
+        }
     }
 }
