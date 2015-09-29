428a653d7ec0ab4a66f0f4c8c595441c510f2e36
Jens Hunt <himself@jenshunt.se>
2015-09-29 19:23:50 +0200

    Anrop hela vägen till banken för att hämta transaktioner

--- Bankomat/ATM/GetBalance.aspx.cs
+++ Bankomat/ATM/GetBalance.aspx.cs
@@ -15,17 +15,84 @@ namespace ATM
         {
             theAtm = new ATM();
             cardNumber = 12345;
-            LabelBalance.Text = theAtm.GetBalance(cardNumber).ToString(); // Vadå kortnummer????
+            try
+            {
+                LabelBalance.Text = theAtm.GetBalance(cardNumber).ToString(); // Vadå kortnummer????
+            }
+            catch (Exception ex)
+            {
+                LabelBalance.Text = ex.Message; 
+                
+            }
+
+
+            
 
         }
 
         protected void button1Right_Click(object sender, EventArgs e)
         {
+            GetFiveTransaction();
+        }
+
+        private void GetFiveTransaction()
+        {
+            try
+            {
+
+                List<string> theTransactions = new List<string>();//theAtm.GetTransactions(cardNumber, 5);
+
+                PanelTransactions.Controls.Add(new LiteralControl("<div class='transactions'>")); // Css-referens!!!
+                foreach (var transaction in theTransactions)
+                {
+                    Label theTransaction = new Label();
+                    //theTransaction.ID = transaction.;
+                    //theTransaction.Text = transaction.;
+                    theTransaction.CssClass = "singleTransaction";
+                    PanelTransactions.Controls.Add(theTransaction);
+                    PanelTransactions.Controls.Add(new LiteralControl("<br />"));
+
+                }
+                PanelTransactions.Controls.Add(new LiteralControl("</div>"));
+
+
+            }
+            catch (Exception ex)
+            {
+                LabelBalance.Text = ex.Message;
+
+            }
 
         }
+    
+
         protected void button2Right_Click(object sender, EventArgs e)
         {
+            try
+            {
 
+                List<string> theTransactions = new List<string>();//theAtm.GetTransactions(cardNumber, 25);
+
+                PanelTransactions.Controls.Add(new LiteralControl("<div class='transactions'>")); // Css-referens!!!
+                foreach (var transaction in theTransactions)
+                {
+                    Label theTransaction = new Label();
+                    //theTransaction.ID = transaction.;
+                    //theTransaction.Text = transaction.;
+                    theTransaction.CssClass = "singleTransaction";
+                    PanelTransactions.Controls.Add(theTransaction);
+                    PanelTransactions.Controls.Add(new LiteralControl("<br />"));
+
+                }
+                PanelTransactions.Controls.Add(new LiteralControl("</div>"));
+
+
+            }
+            catch (Exception ex)
+            {
+                LabelBalance.Text = ex.Message;
+
+            }
         }
         protected void button3Right_Click(object sender, EventArgs e)
         {
