7f1f401db650216c32fc40b3979f58c9711d2a89
Jens Hunt <himself@jenshunt.se>
2015-09-29 19:46:58 +0200

    polly

--- Bankomat/ATM/Site.Master.designer.cs
+++ Bankomat/ATM/Site.Master.designer.cs
@@ -155,5 +155,14 @@ namespace ATM {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button3;
+        
+        /// <summary>
+        /// PanelRecipe control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel PanelRecipe;
     }
 }
